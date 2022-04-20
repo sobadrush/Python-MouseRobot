2fd08b460b06a7dd128cedac38b6fc8883f4a5c6
Roger Lo <sobadrush@icloud.com>
2022-04-20 16:24:59 +0800

    完成滑鼠機器人-笛卡兒心臟線

--- MouseRobot.py
+++ MouseRobot.py
@@ -4,27 +4,28 @@ from math import cos, pi, sin, radians
 import pyautogui
 import time
 
+"""
+計算【笛卡兒心臟】線點資料
+"""
 def calcDescartesPosXY(radius, deg):
     deg = radians(deg)
-    posX = 3 * radius * (cos(deg) - 0.5 * cos(2 * deg))
-    posY = 3 * radius * (sin(deg) - 0.5 * sin(2 * deg))
-    return { "X": posX, "Y": posY }
+    posX = 1 * radius * (1 * sin(deg) + 0.5 * sin(2 * deg))
+    posY = 1 * radius * (1 * cos(deg) + 0.5 * cos(2 * deg))
+    return {"X": posX, "Y": posY}
+
 
 if __name__ == '__main__':
 
     ww = pyautogui.size().width
     hh = pyautogui.size().height
+    centralX = ww/2
+    centralY = hh/2
 
     # 將游標置中
-    pyautogui.moveTo(x=ww/2, y=hh/2, duration=0)
+    pyautogui.moveTo(x=centralX, y=centralY, duration=0)
 
-    # while True:
-    #     pyautogui.moveTo(x=122, y=224, duration=1.2)
     while True:
-        for deg in range(0, 360, 1):
+        for deg in range(0, 360, step=12):
             pos = calcDescartesPosXY(100, deg)
-            print(pos["X"] + ww/2, pos["Y"] + hh/2)
-            pyautogui.moveTo(x=pos["X"]+ww/2, y=pos["Y"]+hh/2, duration=0)
-        
-     
-
+            print(pos["X"] + centralX, pos["Y"] + centralY)
+            pyautogui.moveTo(x=pos["X"]+centralX, y=pos["Y"]+centralY, duration=0) # 移動滑鼠
