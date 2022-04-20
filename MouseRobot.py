2b9ed3a0f29be83fc93e408316f23ab5cc4711ba
sobadrush <sobadrush@icloud.com>
2022-04-20 20:03:13 +0800

    效能優化, 謹記算一次所有點資料

--- MouseRobot.py
+++ MouseRobot.py
@@ -13,6 +13,16 @@ def calcDescartesPosXY(radius, deg):
     posY = 1 * radius * (1 * cos(deg) + 0.5 * cos(2 * deg))
     return {"X": posX, "Y": posY}
 
+"""
+計算【笛卡兒心臟】線點資料(0~360)
+"""
+def calcDescartesPoints():
+    t = tuple()
+    for deg in range(0, 360, 10): # step = 10
+        posDict = calcDescartesPosXY(100, deg)
+        t += (posDict,) # tuple只能加入tuple → 參考:建立只有一個元素的元組 tuple
+    return t
+
 
 if __name__ == '__main__':
 
@@ -24,8 +34,13 @@ if __name__ == '__main__':
     # 將游標置中
     pyautogui.moveTo(x=centralX, y=centralY, duration=0)
 
+    # 計算一圈心臟線的點資料
+    pointsTuple = calcDescartesPoints()
+
+    # Loop Run
     while True:
-        for deg in range(0, 360, step=12):
-            pos = calcDescartesPosXY(100, deg)
-            print(pos["X"] + centralX, pos["Y"] + centralY)
-            pyautogui.moveTo(x=pos["X"]+centralX, y=pos["Y"]+centralY, duration=0) # 移動滑鼠
+        for i in range(0, len(pointsTuple)):
+            pos_X = pointsTuple[i].get("X") + centralX
+            pos_Y = pointsTuple[i].get("Y") + centralY
+            print(f'{{ {pos_X}, {pos_Y} }}')
+            pyautogui.moveTo(x=pos_X, y=pos_Y, duration=0.01) # 移動滑鼠
